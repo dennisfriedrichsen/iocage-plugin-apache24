673abeed8edcb9c24273529a08de68c00b73860c
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2021-01-10 10:28:32 -0600

    better flesh out the post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+# Enable the service
 sysrc apache24_enable="YES"
-service apache24 start
+
+# Start the service
+if $(service apache24 start 2>/dev/null >/dev/null) ; then
+    echo "Starting apache24."
+fi
